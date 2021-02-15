c52209af87567469ea7c08aba26a09ca01b67443
Carlos Sanchez <caaugustoss@gmail.com>
2021-02-15 18:49:18 +0100

    UNG-1358 Adding scopes model

---
+++ src/main/scala/com/ubirch/models/Scopes.scala
@@ -0,0 +1,57 @@
+package com.ubirch.models
+
+sealed trait Resource
+object Resource {
+  final val list: List[Resource] = List(UPP, Thing)
+  def fromString(value: String): Option[Resource] = {
+    list.find(_.toString.toLowerCase == value)
+  }
+}
+case object UPP extends Resource
+case object Thing extends Resource
+
+sealed trait Action
+object Action {
+  final val list: List[Action] = List(Anchor, Verify, Create)
+  def fromString(value: String): Option[Action] = {
+    list.find(_.toString.toLowerCase == value)
+  }
+}
+case object Anchor extends Action
+case object Verify extends Action
+case object Create extends Action
+
+object Scopes {
+
+  type Scope = (Resource, Action)
+  final val UPP_Anchor: Scope = (UPP, Anchor)
+  final val UPP_Verify: Scope = (UPP, Verify)
+  final val Thing_Create: Scope = (Thing, Create)
+  final val list = List(UPP_Anchor, UPP_Verify, Thing_Create)
+
+  def asString(scope: Scope): String =  {
+    val (resource, action) = scope
+    s"${resource.toString.toLowerCase}:${action.toString.toLowerCase}"
+  }
+
+  def fromString(value: String): List[Scope] = {
+    value
+      .split(",")
+      .toList
+      .map(_.split(":").toList)
+      .flatMap {
+        case List(r, a) =>
+          for {
+            resource <- Resource.fromString(r)
+            action <- Action.fromString(a)
+            scope <- list.find(x => x == (resource, action))
+          } yield {
+            scope
+          }
+        case _ => Nil
+      }
+  }
+
+}
+
+
