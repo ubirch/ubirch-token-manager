64edcd607a6c690eadb0f5fca99040c6d6beab15
Carlos Sanchez <caaugustoss@gmail.com>
2021-02-19 13:38:17 +0100

    UNG-1358 Removing useless code

--- src/main/scala/com/ubirch/services/jwt/TokenDecodingService.scala
+++ src/main/scala/com/ubirch/services/jwt/TokenDecodingService.scala
@@ -17,14 +17,6 @@ trait TokenDecodingService {
 @Singleton
 class DefaultTokenDecodingService @Inject() (jsonConverterService: JsonConverterService) extends TokenDecodingService with LazyLogging {
 
-  final val ISSUER = "iss"
-  final val SUBJECT = "sub"
-  final val AUDIENCE = "aud"
-  final val EXPIRATION = "exp"
-  final val NOT_BEFORE = "nbf"
-  final val ISSUED_AT = "iat"
-  final val JWT_ID = "jti"
-
   def decodeAndVerify(jwt: String, publicKey: PublicKey): Option[JValue] = {
     (for {
       (_, p, _) <- Jwt.decodeRawAll(jwt, publicKey, Seq(JwtAlgorithm.ES256))
@@ -35,14 +27,8 @@ class DefaultTokenDecodingService @Inject() (jsonConverterService: JsonConverter
     } yield {
       Some(all)
     }).recover {
-      case e: InvalidSpecificClaim =>
-        logger.error(s"invalid_token_specific_claim=${e.getMessage}", e)
-        None
       case e: InvalidAllClaims =>
         logger.error(s"invalid_token_all_claims=${e.getMessage}", e)
-        None
-      case e: InvalidOtherClaims =>
-        logger.error(s"invalid_token_other_claims=${e.getMessage}", e)
         None
       case e: Exception =>
         logger.error(s"invalid_token=${e.getMessage}", e)
