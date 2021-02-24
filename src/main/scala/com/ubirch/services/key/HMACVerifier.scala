b26345ca9b7befe3869456f4f5a06d2c9e334a56
Carlos Sanchez <caaugustoss@gmail.com>
2021-02-24 11:48:14 +0100

    UNG-1358 Adding time check so that reqs are not so old

--- src/main/scala/com/ubirch/services/key/HMACVerifier.scala
+++ src/main/scala/com/ubirch/services/key/HMACVerifier.scala
@@ -5,6 +5,7 @@ import java.util.Date
 
 import com.ubirch.models.VerificationRequest
 import com.ubirch.services.state.SecretKeyPoolService
+import org.joda.time.{ DateTime, Duration }
 
 import javax.crypto.SecretKey
 import javax.inject.{ Inject, Singleton }
@@ -17,15 +18,18 @@ trait HMACVerifier {
 class DefaultHMACVerifier @Inject() (secretKeyPoolService: SecretKeyPoolService, HMAC: HMAC) extends HMACVerifier {
 
   def verify(verificationRequest: VerificationRequest): Boolean = {
-    secretKeyPoolService.getKey(verificationRequest.sigPointer)
-      .map { key =>
-        HMAC.getHMAC(
-          verificationRequest.signed.getOrElse("").getBytes(StandardCharsets.UTF_8),
-          verificationRequest.time.map(x => new Date(x.toLong)).getOrElse(new Date()),
-          key.asInstanceOf[SecretKey]
-        )
-      }.contains(verificationRequest.signature)
-
+    (for {
+      reqTime <- verificationRequest.time.map(x => new Date(x.toLong))
+      secsInBetween <- Option(new Duration(new DateTime(reqTime), new DateTime()).abs().getStandardSeconds)
+      signed <- verificationRequest.signed.map(_.getBytes(StandardCharsets.UTF_8))
+      key <- secretKeyPoolService.getKey(verificationRequest.sigPointer)
+    } yield {
+      (HMAC.getHMAC(
+        signed,
+        reqTime,
+        key.asInstanceOf[SecretKey]
+      ) == verificationRequest.signature) && secsInBetween < 120
+    }).getOrElse(false)
   }
 
 }
